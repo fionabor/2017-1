28f20d9a32800039f6954515474af8a708325d76
Fiona Bor <fionabor@seattleacademy.org>
2020-02-05 09:33:20 -0800

    add server pages

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 1505
+const port = 1500
 let counter = 0
 
-app.get('/', (req, res) => res.send('Hello Fiona!'))
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.send('Hello Gary!'))
 
 app.get('/counter', function (req, res) {
 	counter = counter + 1;
   res.send("counter " + counter);
+})
+
+app.get('/date', function (req, res) {
+	let d = new Date();
+	console.log(d);
+  res.send("Date " + d);
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
